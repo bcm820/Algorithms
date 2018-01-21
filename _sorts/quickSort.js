538a30a2a5f9069ac15222f4efb96fc056fd6457
Brian Mendoza <31445238+bcmendoza@users.noreply.github.com>
2018-01-20 19:30:29 -0500

    Update quickSort.js

--- _sorts/quickSort.js
+++ _sorts/quickSort.js
@@ -1,21 +1,32 @@
-/* not yet complete */
-
-let arr = [3,5,8,1,2,9,4,7,6];
 
 function quickSort(arr) {
-  const rQuickSort = (arr, pivot) => {
-    let mL, mR;
-    for (let i = 0, j = arr.length-2; i !== j; i++, j--) {
-      if (arr[i] < pivot) mL = i;
-      if (arr[j] >= pivot) mR = j;
-      if (mL && mR) {
-        [arr[mL], arr[mR]] = [arr[mR], arr[mL]];
-        mL, mR = undefined;
+  
+  const partition = (arr, left, right) => {
+    const pivot = arr[Math.floor((right + left) / 2)];
+    while (left <= right) {
+      if (arr[left] < pivot) left++;
+      if (arr[right] > pivot) right--;
+      if (left <= right) {
+        [arr[left], arr[right]] = [arr[right], arr[left]];
       }
     }
-    [arr[pivot], arr[i]] = [arr[i], arr[pivot]];
+    return left;
+  }
+  
+  const rQuickSort = (arr, left, right) => {
+    if (arr.length > 1) {
+      idx = partition (arr, left, right);
+      if (left < idx - 1) rQuickSort (arr, left, idx - 1);
+      if (idx < right) rQuickSort (arr, idx, right);
+    }
     return arr;
   }
-  rQuickSort(arr);
-  let left = 
+  
+  rQuickSort(arr, 0, arr.length-1);
+  
 }
+  
+
+let arr = [3,5,8,1,2,9,4,7,6];
+quickSort(items);
+console.log(arr);
