26263cdd6c4e5989f81020db52ebdfcbb68aeb46
bcmendoza <31445238+bcmendoza@users.noreply.github.com>
2018-01-24 14:19:44 -0500

    added isCryptSolution

--- _codeFights_interviews.js
+++ _codeFights_interviews.js
@@ -97,3 +97,27 @@ function sudoku2(grid) {
   
   return true;
 }
+
+/* isCryptSolution
+You have an array of strings crypt, the cryptarithm, and an an array containing the mapping of letters and digits, solution. The array crypt will contain three non-empty strings that follow the structure: [word1, word2, word3], which should be interpreted as the word1 + word2 = word3 cryptarithm.
+If crypt, when it is decoded by replacing all of the letters in the cryptarithm with digits using the mapping in solution, becomes a valid arithmetic equation containing no numbers with leading zeroes, the answer is true. If it does not become a valid arithmetic solution, the answer is false.
+*/
+
+function isCryptSolution(crypt, solution) {
+ let sol = {}
+ solution.forEach(el => sol[el[0]] = el[1])
+ 
+ let nums = []
+ crypt.forEach(word => {
+  let str = "";
+  for (let i = 0; i < word.length; i++) { 
+   str+= sol[word[i]]
+  }
+  let int = parseInt(str)
+  if(str.length > 1 && str[0] === "0") return false
+  nums.push(int)
+ })
+ 
+ if(nums[0] + nums[1] === nums[2]) return true
+ return false
+}
